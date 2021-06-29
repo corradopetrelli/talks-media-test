2fa5357424d01236a767790950e06b41bebcbaeb
Corrado Petrelli <corrado.petrelli@gmail.com>
2021-06-29 20:58:21 +0200

    Moved mock data from TableComponent to PageComponent

--- src/components/PageComponent.vue
+++ src/components/PageComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Talks media test</h1>
-  <TableComponent/>
+  <TableComponent :tableData="tableData"/>
 </template>
 
 <script>
@@ -10,6 +10,28 @@ export default {
   name: 'PageComponent',
   components: {
     TableComponent
+  },
+
+  data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }]
+      }
   }
 }
 </script>
--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -69,25 +69,10 @@
         input: ref('')
       }
     },
-    data() {
-      return {
-        tableData: [{
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles'
-        }, {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles'
-        }, {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles'
-        }, {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles'
-        }]
+    props: {
+      tableData: {
+        required: true,
+        type: Object
       }
     },
     methods: {
