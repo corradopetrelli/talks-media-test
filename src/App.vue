d8beec896350633dccec6409c5b5809ffe2f9025
Corrado Petrelli <corrado.petrelli@gmail.com>
2021-06-29 20:46:50 +0200

    Introduced skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>Talks media test</h1>
+  <HelloWorld/>
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,101 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+
+
+  <el-input
+      placeholder="Search"
+      prefix-icon="el-icon-search"
+      v-model="input"
+      clearable>
+  </el-input>
+  <el-button type="primary" icon="el-icon-search">Search</el-button>
+
+
+  <el-dropdown>
+    <el-button type="primary">
+      Dropdown List<i class="el-icon-arrow-down el-icon--right"></i>
+    </el-button>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>Action 1</el-dropdown-item>
+        <el-dropdown-item>Action 2</el-dropdown-item>
+        <el-dropdown-item>Action 3</el-dropdown-item>
+        <el-dropdown-item>Action 4</el-dropdown-item>
+        <el-dropdown-item>Action 5</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+ 
+
+  <el-table
+    :data="tableData"
+    :default-sort = "{prop: 'date', order: 'descending'}"
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="Date"
+      sortable>
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="Name"
+      sortable>
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="Address"
+      sortable
+      :formatter="formatter">
+    </el-table-column>
+  </el-table>
+
+
+  <div class="block">
+    <el-pagination
+      layout="prev, pager, next, total"
+      background
+      :page-sizes="[10,20,40,80]"
+      :page-size="10"
+      :total="1000">
+    </el-pagination>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
+
+  import { defineComponent, ref } from 'vue'
+
+  export default {
+    name: 'HelloWorld',
+    setup() {
+      return {
+        input: ref('')
+      }
+    },
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }]
+      }
+    },
+    methods: {
+      formatter(row, column) {
+        return row.address;
+      }
     }
   }
-}
 </script>
