6dfaf9058cdf9e212fba89f48485792447e2a6bf
Corrado Petrelli <corrado.petrelli@gmail.com>
2021-06-30 16:37:34 +0200

    Setup eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
-    "root": true,
-    "extends": [
-      "plugin:vue/essential",
-      "plugin:prettier/recommended",
-      "eslint:recommended"
-    ],
-  }
+  root: true,
+  extends: ["plugin:vue/vue3-recommended", "prettier", "prettier/vue", "eslint:recommended"],
+  rules: {
+    // Override some rules
+  }, 
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+};
